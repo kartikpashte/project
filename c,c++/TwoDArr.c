2566b61d4c8c2e1b2abcad5aa5b135e1626de929
kartikpashte <kartikpashte58@gmail.com>
2025-03-15 11:29:00 +0000

    upgrade

---
+++ c,c++/TwoDArr.c
@@ -0,0 +1,79 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int a[3][3];
+//     int b[3][3];
+
+//     int i, j;
+
+//     for (i = 0; i < 3; i++)
+//     {
+//         for (j = 0; j < 3; j++)
+//         {
+//             printf("Enter value for Matrix[%d][%d]:", i, j);
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+
+//     for (i = 0; i < 3; i++)
+//     {
+//         for (j = 0; j < 3; j++)
+//         {
+//             printf("Enter value for Matrix[%d][%d]:", i, j);
+//             scanf("%d", &b[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     for (i = 0; i < 3; i++)
+//     {
+//         for (j = 0; j < 3; j++)
+//         {
+//             printf("%d\t", a[i][j] + b[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a[3][3];
+//     int b[3][3];
+
+//     int i, j;
+
+//     for (i = 0; i < 3; i++)
+//     {
+//         for (j = 0; j < 3; j++)
+//         {
+//             printf("Enter value for Matrix[%d][%d]:", i, j);
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+
+//     for (i = 0; i < 3; i++)
+//     {
+//         for (j = 0; j < 3; j++)
+//         {
+//             printf("Enter value for Matrix[%d][%d]:", i, j);
+//             scanf("%d", &b[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     for (i = 0; i < 3; i++)
+//     {
+//         for (j = 0; j < 3; j++)
+//         {
+//             printf("%d\t", a[i][j] * b[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
