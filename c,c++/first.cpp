3f70189244a30e1d94c83cb87b48a9078a972551
kartikpashte <kartikpashte58@gmail.com>
2025-04-16 10:49:13 +0000

    upgrade

---
+++ c,c++/first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// int main() {
+//     std::cout << "Hello, world!" ;
+//     return 0;
+// }
+
+int main() {
+    int a = 10;
+    cout << "Hello, world!" << endl << "a = " << a << endl;
+}
